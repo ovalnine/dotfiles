6693fab24abf41d8faab8edde4e996f7b18d821d
Ivan Leon <ivanleonaguilar@gmail.com>
2023-12-28 15:32:21 -0800

    A bunch of theme changes

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -2,10 +2,8 @@
 
 # Terminate already running bar instances
 killall -q polybar
-# If all your bars have ipc enabled, you can also use
-# polybar-msg cmd quit
 
 # Launch Polybar, using default config location ~/.config/polybar/config.ini
-polybar example 2>&1 | tee -a /tmp/polybar.log & disown
+polybar main
 
 echo "Polybar launched..."
